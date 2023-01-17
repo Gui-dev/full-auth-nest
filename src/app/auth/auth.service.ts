77438b881fcf353ce2220495730832e65b513da4
Gui-dev <adrianon10@gmail.com>
2023-01-17 03:50:45 -0300

    feat: add helper method signup

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common'
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
+
 
 import { SignupDTO } from '../dtos/SignupDTO'
+import { HashedPassword } from '../utils/HashedPassword'
 import { PrismaService } from './../database/prisma/prisma.service'
 
 
@@ -10,11 +12,25 @@ export class AuthService {
     private readonly prisma: PrismaService
   ) { }
 
-  public async signup ({ email, password }: SignupDTO) {
-    return {
-      email,
-      password
+  public async signup ({ email, password }: SignupDTO): Promise<void> {
+    const foundUser = await this.prisma.user.findUnique({
+      where: {
+        email
+      }
+    })
+    if (foundUser) {
+      throw new HttpException({
+        status: HttpStatus.CONFLICT,
+        error: 'User already exists'
+      }, HttpStatus.CONFLICT)
     }
+    const hashedPassword = await HashedPassword.hashPassword(password)
+    await this.prisma.user.create({
+      data: {
+        email,
+        hashedPassword
+      }
+    })
   }
 
   public async signin () {
