3be307c65066bba8d784637e9c7acae95b09d3c4
Gui-dev <adrianon10@gmail.com>
2023-01-18 04:23:53 -0300

    feat: add UserModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AuthModule } from './app/auth/auth.module'
 import { PrismaModule } from './app/database/prisma/prisma.module'
+import { UserModule } from './app/user/user.module'
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [AuthModule, PrismaModule, UserModule],
   controllers: [],
   providers: []
 })
