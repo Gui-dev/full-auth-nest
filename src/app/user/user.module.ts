ae93dfec9cf707617f2deadf20cd796ae9ecf143
Gui-dev <adrianon10@gmail.com>
2023-01-19 03:36:32 -0300

    feat: add JwtStrategy provider

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
+
 import { UserService } from './user.service'
 import { UserController } from './user.controller'
+import { JwtStrategy } from '../auth/jwt.strategy'
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService, JwtStrategy]
 })
 export class UserModule { }
