9ae2e6553e89e1d7569b051a34154267b673ae61
Gui-dev <adrianon10@gmail.com>
2023-01-19 03:19:17 -0300

    feat: add PassportModule

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common'
-import { AuthService } from './auth.service'
-import { AuthController } from './auth.controller'
+import { PassportModule } from '@nestjs/passport'
 import { JwtModule } from '@nestjs/jwt'
 
+import { AuthService } from './auth.service'
+import { AuthController } from './auth.controller'
+
 @Module({
-  imports: [JwtModule],
+  imports: [JwtModule, PassportModule],
   controllers: [AuthController],
   providers: [AuthService]
 })
